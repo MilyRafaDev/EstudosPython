fb5b683617e7de1e4e53bae06aa6fc7481826e5d
Mily Novaski <mihrafa.dev@gmail.com>
2025-03-18 08:16:13 -0300

    Add files via upload
    Programa que pede dois números ao usuário e retorna os resultados de adição, subtração divisão e multiplicação

---
+++ SDAS.py
@@ -0,0 +1,15 @@
+n1= float(input('Digite o numero X: '))
+n2= float(input('Digite o numero Y: '))
+#terminal do usuário
+print('_____________________________________________________________________________')
+soma = n1 + n2
+print('Adição: {} + {} = {}'.format(n1,n2,soma))
+menos = n1 - n2
+print('Subtração: {} - {} = {}'.format(n1,n2,menos))
+vezes = n1 * n2
+print('Multiplicação: {} X {} = {:.2f}'.format(n1,n2,vezes))
+Dividir = n1 / n2
+print('Divisão: {} / {} = {:.2f}'.format(n1,n2,Dividir))
+print('_____________________________________________________________________________')
+
+
